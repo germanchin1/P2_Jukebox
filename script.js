c45082654e5e1c9ab42fef942981e8f76f2f5c6a
german <germanromo2006@gmail.com>
2025-10-18 15:37:24 +0200

    boton siguiente, check para no reproducir 2 songs a la vez

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-let ruta_cancion = "songs\\song1.mp3"
-let boton_play = document.getElementById("botonPlay")
+let n_cancion = 1;
+let ruta_cancion = `songs/song${n_cancion}.mp3`;
+let boton_play = document.getElementById("botonPlay");
 
-var sound = new Howl({
-    src: [ruta_cancion],
-    autoplay: false,
-    loop: false,
-});
+let sound;
 
+function empezarMusica() {
 
 
-function empezarMusica(){
+    ruta_cancion = `songs/song${n_cancion}.mp3`;
+
+    if (sound) {
+        sound.stop();
+    }
+
+    sound = new Howl({
+        src: [ruta_cancion],
+        autoplay: false,
+        loop: false,
+    });
+
+
     sound.play();
-    console.log("play")
-    console.log(sound)
-    
+    console.log("Reproduciendo: ", ruta_cancion);
 }
-function pararMusica(){
+
+function pararMusica() {
+
     sound.pause();
-    console.log("stop")
+    console.log("stop");
+}
+
+
+function siguienteCancion() {
+    sound.stop();
+    n_cancion++;
+    console.log("Poniendo: ", n_cancion);
+    empezarMusica();
 }
